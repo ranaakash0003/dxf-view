b2551e3ad2f6526247083159c7fdef1fe66b71b0
Rana Akash <rana.nitex@gmail.com>
2023-12-14 18:18:00 +0600

    add line and arc view for the canvas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './App.css';
-import DXFViewer from './DXFViewer'
-import dxfFilePath  from './asset/test.dxf'
+import React from "react";
+import "./App.css";
+import DXFViewer from "./DXFViewer";
+import dxfFilePath from "./asset/test.dxf";
 
 function App() {
-  
-  return (
-    <div className="App">
-      <h1>DXF Viewer</h1>
-      <DXFViewer dxfFilePath={dxfFilePath} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>DXF Viewer</h1>
+            <DXFViewer dxfFilePath={dxfFilePath} />
+        </div>
+    );
 }
 
 export default App;
--- src/DXFViewer.js
+++ src/DXFViewer.js
@@ -1,54 +1,70 @@
-import React, { useEffect } from 'react';
-import DXFParser from 'dxf-parser';
+import React, { useEffect } from "react";
+import DXFParser from "dxf-parser";
 
 const DXFViewer = ({ dxfFilePath }) => {
-  useEffect(() => {
-    const canvas = document.getElementById('dxf-canvas');
-    const ctx = canvas.getContext('2d');
+    useEffect(() => {
+        let canvas = document.getElementById("dxf-canvas");
 
-    fetch(dxfFilePath)
-      .then((response) => response.text())
-      .then((data) => {
-        if (data) {
-          const parser = new DXFParser();
-          const dxfData = parser.parse(data);
-
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-          console.log('DXF Data`````:', dxfData);
-
-
-          dxfData.entities.slice(0,6).forEach((entity,idx) => {
-            console.log('hhhhhhhhhhh',idx);
-            if (entity.type === 'LINE') {
-              const start = entity.vertices[0];
-              const end = entity.vertices[1];
-              ctx.beginPath();
-              ctx.moveTo(start.x, start.y);
-              ctx.lineTo(end.x, end.y);
-              ctx.strokeStyle = 'green';
-              ctx.stroke();
-            }
-            
-            else if (entity.type === 'CIRCLE') {
-              const center = entity.center;
-              const radius = entity.radius;
-              ctx.beginPath();
-              ctx.arc(center.x, center.y, radius, 0, 2 * Math.PI);
-              ctx.strokeStyle = 'blue';
-              ctx.stroke();
-            }
-
-          });
-        
-
-        } else {
-          console.error('No valid entities found in DXF data.');
+        if (!canvas) {
+            console.error("Canvas not found in the DOM.");
+            return;
         }
-      });
-  }, [dxfFilePath]);
 
-  return <canvas id="dxf-canvas" width={window.innerWidth} height={window.innerHeight} />;
+        let ctx = canvas.getContext("2d");
+        // ctx.beginPath();
+        // ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, 2 * Math.PI);
+        // ctx.strokeStyle = "blue";
+        // ctx.lineWidth = 2;
+        // ctx.stroke();
+        // console.log("vvvvvvvvvvv", canvas.width / 2, canvas.height / 2);
+
+        fetch(dxfFilePath)
+            .then((response) => response.text())
+            .then((data) => {
+                if (data) {
+                    const parser = new DXFParser();
+                    const dxfData = parser.parse(data);
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                    console.log("DXF Data:", dxfData);
+
+                    dxfData.entities.forEach((entity, idx) => {
+                        if (entity.type === "LINE") {
+                            const scaleX = 0.2;
+                            const scaleY = 0.2;
+                            const start = entity.vertices[0];
+                            const end = entity.vertices[1];
+                            ctx.beginPath();
+                            ctx.moveTo(start.x * scaleX, start.y * scaleY);
+                            ctx.lineTo(end.x * scaleX, end.y * scaleY);
+                            ctx.strokeStyle = "green";
+                            ctx.stroke();
+                        } else if (entity.type === "ARC") {
+                            const scaleX = 0.1; // Adjust this scaling factor as needed
+                            const scaleY = 0.1; // Adjust this scaling factor as needed
+                            const center = entity.center;
+                            const radius = entity.radius;
+                            // console.log(center.x * scaleX, "vvvvvvvvvvv", Math.abs(center.y) * scaleY);
+                            ctx.beginPath();
+                            ctx.arc(center.x * scaleX, (Math.abs(center.y) * scaleY) / 4, radius, 0, 2 * Math.PI);
+                            ctx.strokeStyle = "blue";
+                            ctx.stroke();
+                        }
+                    });
+                } else {
+                    console.error("No valid entities found in DXF data.");
+                }
+            });
+    }, [dxfFilePath]);
+
+    return (
+        <canvas
+            style={{ backgroundColor: "#f4f0ee" }}
+            id="dxf-canvas"
+            width={window.innerWidth}
+            height={window.innerHeight}
+        />
+    );
 };
 
 export default DXFViewer;
